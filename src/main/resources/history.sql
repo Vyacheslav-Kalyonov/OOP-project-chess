9343443343059e40fc537e9c3eb91404f96aa515
Vyacheslav Kalyonov <kalenoff.slava@yandex.ru>
2023-12-16 12:11:53 +0300

    Добавление запросов  в ресурсы для добавления данных в таблицу

---
+++ src/main/resources/history.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.history (id, time_start, time_end, winner, moves) VALUES (1, '05.11.23', '06.11.23', 'White', 'g2 g4 e2 e4');
+INSERT INTO public.history (id, time_start, time_end, winner, moves) VALUES (2, '05.11.23', '06.11.23', 'Black', 'g2 g4 e2 e4 f2 f4');
+INSERT INTO public.history (id, time_start, time_end, winner, moves) VALUES (3, '2023-11-05T23:38:49.255212100+03:00[Europe/Moscow]', '2023-11-05T23:38:49.255212100+03:00[Europe/Moscow]', 'Black', '[[A2 A4, B2 B4, C2 C4]]');
+INSERT INTO public.history (id, time_start, time_end, winner, moves) VALUES (4, '2023-11-05T23:40:04.879877+03:00[Europe/Moscow]', '2023-11-05T23:40:04.879877+03:00[Europe/Moscow]', 'Black', '[[A2 A4, B2 B4, C2 C4]]');
+INSERT INTO public.history (id, time_start, time_end, winner, moves) VALUES (5, '2023-11-06T00:40:09.854138+03:00[Europe/Moscow]', '2023-11-06T00:41:21.374382500+03:00[Europe/Moscow]', 'White', '[E2 E4, E7 E5, F1 C4, A7 A6, D1 H5, A6 A5, H5 F7]');
+INSERT INTO public.history (id, time_start, time_end, winner, moves) VALUES (6, '2023-11-06T00:42:59.259108700+03:00[Europe/Moscow]', '2023-11-06T00:43:44.835317800+03:00[Europe/Moscow]', 'White', '[E2 E4, E7 E5, D1 H5, A7 A6, F1 C4, A6 A5, H5 F7]');
+INSERT INTO public.history (id, time_start, time_end, winner, moves) VALUES (7, '2023-11-06T11:06:39.086647400+03:00[Europe/Moscow]', '2023-11-06T11:07:31.579711400+03:00[Europe/Moscow]', 'White', '[E2 E4, E7 E5, D1 H5, B7 B6, F1 C4, B6 B5, H5 F7]');
+INSERT INTO public.history (id, time_start, time_end, winner, moves) VALUES (8, '2023-11-12T13:22:51.712870700+03:00[Europe/Moscow]', '2023-11-12T13:23:41.725867500+03:00[Europe/Moscow]', 'White', '[E2 E4, E7 E5, D1 H5, A7 A6, F1 C4, A6 A5, H5 F7]');
